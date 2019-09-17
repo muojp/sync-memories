5317e3724592bd69d44dd035694d7e8c73d3bc6f
Kei Nakazawa <muo@muo.jp>
2019-09-17 08:58:59 -0400

    WIP
     - But getting better

--- app/src/main/java/jp/muo/syncmemories/MainActivity.kt
+++ app/src/main/java/jp/muo/syncmemories/MainActivity.kt
@@ -1,18 +1,87 @@
 package jp.muo.syncmemories
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
+import java.lang.StringBuilder
+import androidx.documentfile.provider.DocumentFile
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.net.Uri
 
+
+
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    companion object {
+        val TAG = "SyncMemories"
     }
 
-    override fun onResume() {
-        super.onResume()
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun acquirePermissions() {
+        Toast.makeText(this, "hey", Toast.LENGTH_SHORT).show()
+        val str = "content://com.android.externalstorage.documents/tree/1EEE-3510%3A"
+        val fileRef = DocumentFile.fromTreeUri(this, Uri.parse(str))?.let {
+            Log.d(TAG, "SD access enabled")
+            if (it.isDirectory) {
+                val d = it.findFile("DCIM")?.let {
+                    if (it.exists()) {
+                        Log.d(TAG, "found DCIM")
+                    }
+                }
+            }
+        }
+        // startActivityForResult(Intent(Intent.ACTION_OPEN_DOCUMENT_TREE), 42);
+    }
+
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+    fun chooseTargetDirectory() {
+        startActivityForResult(Intent(Intent.ACTION_OPEN_DOCUMENT_TREE), 44);
+    }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn_check_permission.setOnClickListener {
+            acquirePermissionsWithPermissionCheck()
+        }
+        btn_choose_target.setOnClickListener {
+            chooseTargetDirectoryWithPermissionCheck()
+        }
         PicturesSyncService.invoke(this)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        onRequestPermissionsResult(requestCode, grantResults)
+    }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, resultData: Intent?) {
+        if (resultCode != Activity.RESULT_OK)
+            return
+        val treeUri = resultData!!.data as Uri
+        Log.d(TAG, treeUri.toString())
+        val pickedDir = DocumentFile.fromTreeUri(this, treeUri)
+        grantUriPermission(
+            packageName,
+            treeUri,
+            Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+        )
+        contentResolver.takePersistableUriPermission(
+            treeUri,
+            Intent.FLAG_GRANT_READ_URI_PERMISSION or Intent.FLAG_GRANT_WRITE_URI_PERMISSION
+        )
     }
 }
--- app/src/main/java/jp/muo/syncmemories/PictureSyncService.kt
+++ app/src/main/java/jp/muo/syncmemories/PictureSyncService.kt
@@ -1,20 +1,33 @@
 package jp.muo.syncmemories
 
 import android.app.*
+import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.graphics.Color
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
 import android.widget.Toast
 import androidx.core.app.JobIntentService
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
+import java.io.File
 
 class PicturesSyncService : JobIntentService() {
     companion object {
+        val USB_DETACH_INTENT = "android.hardware.usb.action.USB_DEVICE_DETACHED"
+        val INJECT_DUMMY = true
+        val TAG = "SyncService"
+        val JOB_ID = 1330
+        val NOTIFICATION_CHANNEL_ID = "progress"
+        val NOTIFICATION_CHANNEL_NAME = "Data Sync progress"
+        val NOTIFICATION_ID = 1
+
         fun invoke(context: Context) {
             val intent = Intent(context, PicturesSyncService::class.java)
-            enqueueWork(context, PicturesSyncService::class.java, 1330, intent)
+            enqueueWork(context, PicturesSyncService::class.java, JOB_ID, intent)
         }
     }
 
@@ -25,32 +38,99 @@ class PicturesSyncService : JobIntentService() {
     }
 
     val notificationManager by lazy { applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager }
-
-    private fun prepareNotification() {
-        val notificationBuilder = NotificationCompat.Builder(applicationContext, "progress").apply {
+    val notificationBuilder by lazy {
+        NotificationCompat.Builder(applicationContext, NOTIFICATION_CHANNEL_ID).apply {
             setAutoCancel(true)
             setDefaults(Notification.DEFAULT_ALL)
             setWhen(System.currentTimeMillis())
             setSmallIcon(R.drawable.ic_launcher_background)
             setContentTitle("Title")
-            setContentText("Text")
             setContentInfo("Info")
         }
-        createNotificationChannel("progress", "Data Sync progress")
-        val notification = notificationBuilder.build()
-        notificationManager.notify(1, notification)
-        startForeground(1, notification)
+    }
+
+    private fun prepareNotification() {
+        createNotificationChannel(NOTIFICATION_CHANNEL_ID, NOTIFICATION_CHANNEL_NAME)
+        val notification = notificationBuilder.apply { setContentText("Text0") }.build()
+        notificationManager.notify(NOTIFICATION_ID, notification)
+        startForeground(NOTIFICATION_ID, notification)
+    }
+
+    private fun updateNotification(op: (NotificationCompat.Builder) -> Unit) {
+        val notification = notificationBuilder.apply(op).build()
+        notificationManager.notify(NOTIFICATION_ID, notification)
+    }
+
+    private fun cleanup() {
+        cleanupNotification()
+        if (receiver != null) {
+            unregisterReceiver(receiver)
+            receiver = null
+        }
     }
 
     private fun cleanupNotification() {
-        notificationManager.cancel(1)
+        notificationManager.cancel(NOTIFICATION_ID)
+    }
+
+    var rootDirs: MutableList<File>? = null
+
+    private fun acquireRootDirs() {
+        val storageFiles = ContextCompat.getExternalFilesDirs(applicationContext, null)
+        storageFiles.forEach { Log.d(TAG, "RawPath: ${it.absolutePath}") }
+        rootDirs = storageFiles.filter { o ->
+            o.isDirectory() && o.totalSpace != 0L &&
+                    o.listFiles()!!.filter { p -> p.isDirectory() && p.name == "DCIM" }.count() == 1
+        }.toMutableList()
+        if (rootDirs!!.count() == 0 && INJECT_DUMMY) {
+            val dummyRoot = File("/storage/emulated/0/dummy")
+            rootDirs?.add(dummyRoot)
+        }
+    }
+
+    private fun isMediaConnected(): Boolean {
+        rootDirs?.forEach { Log.d(TAG, "Path: ${it.absolutePath}") }
+        return rootDirs?.count() != 0
+    }
+
+    var receiver: BroadcastReceiver? = null
+
+    private fun registerReceiverForDetaching() {
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                when (intent.action) {
+                    USB_DETACH_INTENT -> {
+                        unregisterReceiver(this)
+                        cancelSync()
+                    }
+                }
+            }
+        }
+        registerReceiver(receiver, IntentFilter(USB_DETACH_INTENT))
+    }
+
+    private fun cancelSync() {
+        throw Exception("Cancel not implemented")
     }
 
     override fun onHandleWork(intent: Intent) {
         showToast("Starting PictureSyncService.")
+        Thread.sleep(2000)
+        acquireRootDirs()
+        if (!isMediaConnected()) {
+            return
+        }
+        registerReceiverForDetaching()
         prepareNotification()
-        Thread.sleep(5000)
-        cleanupNotification()
+        Thread.sleep(2000)
+        updateNotification({ o ->
+            run {
+                o.setContentText(rootDirs!!.firstOrNull()!!.absolutePath)
+                o.setProgress(100, 3, false)
+            }
+        })
+        Thread.sleep(3000)
+        cleanup()
     }
 
     private fun createNotificationChannel(channelId: String, channelName: String) {
